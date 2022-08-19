43b344411787b7884645b3b263daa2c79f509844
Alangobe <alangobe01@gmail.com>
2022-08-19 15:37:24 -0500

    Implementada lista en el state

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,17 +5,13 @@ import { AddCategory } from './components/AddCategory';
 export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState([ 'One Piece', 'One Punch' ]);
-  
-  const onAddCategory = () => {
-    console.log('tu ta loco papi que pasa');
-    setCategories([...categories, 'Valorant'])
-  }
 
   return (
     <>
       <h1>GifExpertApp</h1>
-      <AddCategory />
-      <button onClick={onAddCategory}>Agregar</button>  
+
+      <AddCategory setCategories={ setCategories }/>
+  
       <ol>
         {categories.map(category => {return <li key={category}> {category} </li>})}
       </ol>
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import { useState } from 'react'
 
-export const AddCategory = () => {
+export const AddCategory = ({ setCategories }) => {
 
     const [inputValue, setInputValue] = useState('One Piece');
 
-    const onInputChange = () => {
-        set
+    const onInputChange = ({ target }) => {
+        // console.log( target.value );
+        setInputValue( target.value );
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputValue);
+        setCategories((categories) => [inputValue, ...categories])
     }
 
     return (
 
-        <input 
-            type='text'
-            placeholder='Buscar gifs'
-            value={ inputValue }
-            onChange
-        />
+        <form onSubmit={((event) => onSubmit(event))}>
+            <input 
+                type='text'
+                placeholder='Buscar gifs'
+                value={ inputValue }
+                onChange={ onInputChange }
+            />
+        </form>
 
     )
 
