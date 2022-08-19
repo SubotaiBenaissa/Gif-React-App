f787f22c38e73aed1aae7d7646b04b72f046c81d
Alangobe <alangobe01@gmail.com>
2022-08-19 13:08:30 -0500

    Creada carpeta components

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react';
 import React from 'react';
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState([ 'One Piece', 'One Punch' ]);
   
   const onAddCategory = () => {
-
+    console.log('tu ta loco papi que pasa');
+    setCategories([...categories, 'Valorant'])
   }
 
   return (
     <>
       <h1>GifExpertApp</h1>
-      <button>Agregar</button>  
+      <AddCategory />
+      <button onClick={onAddCategory}>Agregar</button>  
       <ol>
         {categories.map(category => {return <li key={category}> {category} </li>})}
       </ol>
     </>
-  )
+  );
 
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState } from 'react'
+
+export const AddCategory = () => {
+
+    const [inputValue, setInputValue] = useState('One Piece');
+
+    const onInputChange = () => {
+        set
+    }
+
+    return (
+
+        <input 
+            type='text'
+            placeholder='Buscar gifs'
+            value={ inputValue }
+            onChange
+        />
+
+    )
+
+}
